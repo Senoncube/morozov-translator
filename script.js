1cc67c3e0be82c36bc3be17201509b6527c7a933
OleksandrSavchenko <ipz214_soo@student.ztu.edu.ua>
2022-11-30 19:27:19 +0200

    Upload Morozov Translator 0.0.1

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById('translate').onclick = function () {
+    let code = document.querySelector('textarea').value;
+    let repls = [
+        ['а', 'a'], ['е', 'e'],
+        ['у', 'y'], ['р', 'p'],
+        ['о', 'o'], ['к', 'k'],
+        ['з', '3'], ['х', 'x'],
+        ['с', 'c'], ['т', 't'],
+        ['А', 'A'], ['К', 'K'],
+        ['Е', 'E'], ['Н', 'H'],
+        ['З', '3'], ['Х', 'X'],
+        ['В', 'B'], ['Р', 'P'],
+        ['О', 'O'], ['С', 'C'],
+        ['Т', 'T'], ['І', 'l'],
+    ];
+    repls.forEach(function (repl) {
+        code = code.replaceAll(repl[0], repl[1]);
+    });
+    document.querySelector('textarea').value = code;
+}
+
+document.getElementById('exec').onclick = function () {
+    try {
+        eval(document.querySelector('textarea').value);
+    } catch (e) {
+        alert(e);
+    }
+
+}
+
